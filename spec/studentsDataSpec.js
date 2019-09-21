ab750781853795ad1f695ba67527aff120a31287
wallypark710 <wallypark710@gmail.com>
2019-09-21 11:51:09 +0900

    Edit sprint setting
     - CircleCi를 airCi로 대체

--- spec/studentsDataSpec.js
+++ spec/studentsDataSpec.js
@@ -1,5 +1,4 @@
 const studentData = require("../student.json");
-const review = require("./review.json");
 var chai = require('chai')
 var expect = chai.expect
 
@@ -13,13 +12,5 @@ describe("student", () => {
   it("put correct class on student.json", () => {
     const rawClass = "기수를 숫자만! 입력해주세요! 예)11";
     expect(studentData.theClass).not.to.equal(rawClass);
-  });
-});
-
-describe("REVIEW.md", () => {
-  it("should review on REVIEW.md\nYou should leave comments on the improvements section!!\nBare Minimum을 완료하셨다면 student.json, REVIEW.md를 작성하고 Pull request를 만든 뒤 Advanced 진행부탁드립니다!", function() {
-    expect(review["improvements-comments"]).not.to.equal(
-      '**이 글을 지우고 이곳에 남겨주세요. 남겨주실 내용이 없으면 "없음"이라고 적어주세요!**'
-    );
   });
 });
